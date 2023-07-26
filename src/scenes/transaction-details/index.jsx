c83a7a551431d6e3abf7083ba2653004d5d29020
fabiobr05 <fabiob.rodrigues123@gmail.com>
2023-07-26 00:04:28 -0400

    implementation of description transactions

--- src/scenes/transaction-details/index.jsx
+++ src/scenes/transaction-details/index.jsx
@@ -1,12 +1,16 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import PieChart from "../../components/PieChart";
+import { useParams } from "react-router-dom";
 
 const TransactionDetails = () => {
+
+  const { transactionId } = useParams();
   return (
     <Box m="20px">
       <Header title="Pie Chart" subtitle="Simple Pie Chart" />
       <Box height="75vh">
+        id da transação que vou conectar depois: {transactionId}
         <PieChart />
       </Box>
     </Box>
