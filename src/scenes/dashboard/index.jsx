e0f143b6dd427c69f2ac8b6a9b210765b6c064f8
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2023-07-28 21:59:57 -0400

    fix: show number of parameters on demand

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -273,9 +273,9 @@ const Dashboard = () => {
             fontWeight="600"
             sx={{ padding: "30px 30px 0 30px" }}
           >
-            Top 10 Parâmetros mais importantes
+            {mockBarImportance.length} Parâmetros mais importantes
           </Typography>
-          <Box height="90%" mt="-25px" ml="5px" mr="-80px">
+          <Box height="90%" mt="-25px">
             <BarChart isDashboard={true} data={mockBarImportance} />
           </Box>
         </Box>
