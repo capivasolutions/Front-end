ec40b817097ed5ba97c2e7a49bade3d64a87704c
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2023-07-28 22:15:33 -0400

    fix: adjust line chart

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -1,15 +1,15 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import { tokens } from "../theme";
-// import ProgressCircle from "./ProgressCircle";
 
-const StatBox = ({ title, subtitle, icon, progress, increase }) => {
+const StatBox = ({ title, subtitle, icon, color }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const textColor = color ?? colors.grey[100];
 
   return (
     <Box width="100%" m="0 30px" textAlign="center">
       <Box display="flex" justifyContent="space-between">
-        <Box>
+        <Box display="flex" flexDirection="column" gap="8px">
           {icon}
           <Typography
             variant="h4"
@@ -19,21 +19,11 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
             {title}
           </Typography>
         </Box>
-        {/* <Box>
-          <ProgressCircle progress={progress} />
-        </Box> */}
       </Box>
       <Box display="flex" justifyContent="space-between" mt="2px">
-        <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+        <Typography variant="h5" sx={{ color: textColor }}>
           {subtitle}
         </Typography>
-        {/* <Typography
-          variant="h5"
-          fontStyle="italic"
-          sx={{ color: colors.greenAccent[600] }}
-        >
-          {increase}
-        </Typography> */}
       </Box>
     </Box>
   );
